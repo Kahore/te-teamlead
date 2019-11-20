63519edcb42555559ca4242b356b9dcaaedbf0a8
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-11-20 18:31:15 +0600

    add env

--- src/service/film.ts
+++ src/service/film.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 class FilmService {
   public static async getGenres() {
     const genderList = await axios.get(
-      'https://api.themoviedb.org/3/genre/movie/list?api_key=2f0075bbc35ce91def13d04dfe80888a'
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.VUE_APP_API}`
     );
     return genderList.data.genres;
   }
   public static async getFilms(genreId: number) {
     const filmList = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=2f0075bbc35ce91def13d04dfe80888a&with_genres=${genreId}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.VUE_APP_API}&with_genres=${genreId}`
     );
     return filmList.data.results;
   }
