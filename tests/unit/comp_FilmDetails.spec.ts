4f00c9e317477a3f2b21530f524733767ad30710
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-11-22 10:31:34 +0600

    remove comment test

--- tests/unit/comp_FilmDetails.spec.ts
+++ tests/unit/comp_FilmDetails.spec.ts
@@ -15,35 +15,6 @@ beforeEach(() => {
 
 describe('FilmDetails.vue', () => {
   const filmId = '290859';
-  // it('should display genres', () => {
-  //   const wrapper = shallowMount(FilmDetails, {
-  //     mocks: {
-  //       $route: {
-  //         params: { filmId }
-  //       }
-  //     },
-  //     store,
-  //     localVue,
-  //     attachToDocument: true
-  //   });
-  //   wrapper.setData({
-  //     filmDetails: {
-  //       id: 1,
-  //       poster_path: 'some poster_path',
-  //       original_title: 'some original_title',
-  //       tagline: 'some tagline',
-  //       overview: 'some overview',
-  //       release_date: '2019-11-21',
-  //       runtime: 123,
-  //       budget: 123,
-  //       revenue: 123,
-  //       vote_average: 3,
-  //       genre: []
-  //     }
-  //   });
-  //   console.log('TCL: wrapper', wrapper.html());
-  //   wrapper.destroy();
-  // });
   it('should convert incoming details to spec format', () => {
     const wrapper = shallowMount(FilmDetails, {
       mocks: {
