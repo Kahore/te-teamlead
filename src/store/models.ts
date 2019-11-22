397b69339a9b4dd3d68d53555995b5a5a75599bb
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-11-22 10:08:59 +0600

    eslint disable for some API object key

--- src/store/models.ts
+++ src/store/models.ts
@@ -6,14 +6,18 @@ export interface Genre {
 
 export interface FilmDetailsModel {
   id?: number;
+  // eslint-disable-next-line
   poster_path?: string;
+  // eslint-disable-next-line
   original_title?: string;
   tagline?: string;
   overview?: string;
+  // eslint-disable-next-line
   release_date?: string;
   runtime?: number | null;
   budget?: number | null;
   revenue?: number | null;
+  // eslint-disable-next-line
   vote_average?: number;
   genres?: string[];
   trailers?: TrailerModel[];
